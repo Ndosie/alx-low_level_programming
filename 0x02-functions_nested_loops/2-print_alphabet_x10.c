40f110d0cf1b8eeb9d12cb578ea107f86436b01b
Esther Ndosi <estherndosi24@gmail.com>
2024-01-27 07:38:48 -0800

    Printing alphabets 10 times'

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+/**
+ * print_alphabet_x10 - Prints alphabets 10 times
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	char a;
+
+	n = 1;
+	a = 'a';
+
+	while (n <= 10)
+	{
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
