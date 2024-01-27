f1de22f49089d8292adc66d978c471ca7c902267
Esther Ndosi <estherndosi24@gmail.com>
2024-01-27 08:48:03 -0800

    Checking if the character is alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,11 @@
-#include "main.h"#include "main.h"#include "main.h"
+#include "main.h"
+/**
+ * _isalpha - Checks if the character is alphabet
+ * @c: The character to check
+ *
+ * Return: 1 if the character is alphabet and zero if the character is not
+ */
+int _isalpha(int c)
+{
+	return (c >= 'A' && c <= 'z');
+}
