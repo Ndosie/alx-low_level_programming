3071bab4e7c4a9233f6c3e40cd7a6fd9f5cac1ff
Esther Ndosi <estherndosi24@gmail.com>
2024-01-27 09:41:03 -0800

    Prints an absolute of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute of an integer
+ * @n: an integer to compute its absolute
+ *
+ * Return: integer
+ */
+int _abs(int n)
+{
+	return (-n);
+}
