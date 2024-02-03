2e57273c90d651c97d9763883050bc1b3d948606
Esther Ndosi <estherndosi24@gmail.com>
2024-02-03 00:46:27 -0800

    Print table of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * time_table - Prints table of 9
+ *
+ * Return: Void
+ */
+voide time_table(void)
+{
+	int r;
+	int c;
+	int m;
+
+	r = 0;
+
+	while (r < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			m = r * c;
+			if (m > 9)
+			{
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+			}
+			else
+			{
+				_putchar(m + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+			c++;
+		}
+		_putchar('\n');
+		r++;
+	}
+}
