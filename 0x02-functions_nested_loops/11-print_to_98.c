76403277c9f6c8f26a4ddfe50282bcdc09c6b919
Esther Ndosi <estherndosi24@gmail.com>
2024-02-05 01:04:06 -0800

    Print numbers up to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_to_98 - receives and print it to 98
+ * @n: number to start from
+ *
+ * Return: nothing
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i == n; i <= 98; i++)
+	{
+		if (i > 9)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+}
