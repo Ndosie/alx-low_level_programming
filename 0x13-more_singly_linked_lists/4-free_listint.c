3668e7420524fb12479067331b91c2bb0d250103
Esther Ndosi <estherndosi24@gmail.com>
2024-02-08 00:42:36 -0800

    Free memmory of a linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees the memmory taken by the linked list
+ * @head: linked list to free memmory
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *rem;
+
+	while (head)
+	{
+		rem = head->next;
+		free(head->n);
+		free(head);
+		head = org;
+	}
+}
