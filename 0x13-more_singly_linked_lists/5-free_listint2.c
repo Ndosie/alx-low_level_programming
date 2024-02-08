c57f65a59baf69a7c3624cac69b9e51d86bf09ef
Esther Ndosi <estherndosi24@gmail.com>
2024-02-08 01:39:53 -0800

    Freeing the memmory of a linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the memory of linked list
+ * @head: pointer to the list to free
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *rem;
+
+	while (*head)
+	{
+		rem = *head->next;
+		free(*head);
+		*head = rem;
+	}
+	*head = NULL;
+}
