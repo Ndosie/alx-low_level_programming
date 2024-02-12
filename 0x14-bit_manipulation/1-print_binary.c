5c55fffbcb41f9130b29d3c79ae0f10d8d3d2a63
Esther Ndosi <estherndosi24@gmail.com>
2024-02-12 02:02:36 -0800

    Bitwise change decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - changes decimal number to binary
+ * @n: decimal number to change
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int t;
+	int s;
+
+	t = n;
+	s = 0;
+
+	while (t > 1)
+	{
+		t >> 1;
+		s++;
+	}
+
+	while (s >= 0)
+	{
+		t = n;
+		_putchar(((t >> s) & 1) + '0');
+		s--;
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,10 @@
 #ifndef BITS_MAN
 #define BITS_MAN
 
+#include <stdio.h>
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif
