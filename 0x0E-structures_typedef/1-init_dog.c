ae517690e60e061d0f4e54ed8c524be048000772
Esther Ndosi <estherndosi24@gmail.com>
2024-02-13 22:59:12 -0800

    Initilizing dog data structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes struct dog
+ * @d: struct to initialize
+ * @name: name of a dog
+ * @age: age of a dog
+ * @owner: owner of a dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
