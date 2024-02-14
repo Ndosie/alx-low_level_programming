fe460c715a2c0425dba1242dc0547a33fc55ef06
Esther Ndosi <estherndosi24@gmail.com>
2024-02-13 22:31:21 -0800

    Dog data structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - is a data structure which defines dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @ower: owner of the dog
+ *
+ * Description: Defines the characteristics of the dog
+ */
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+}
