5c0e6b14a1ed36b8f0fdc33e2f19de8638b7ec38
Esther Ndosi <estherndosi24@gmail.com>
2024-02-14 11:04:48 -0800

    Checking if the letter is upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - test the character if is upper case
+ * @c: charater to test
+ *
+ * Return: zero or one
+ */
+int _isupper(int c)
+{
+	int i;
+
+	i = isupper(c);
+
+	return (i);
+}
