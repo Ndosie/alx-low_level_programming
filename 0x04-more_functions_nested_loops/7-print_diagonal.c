31d07fc9bc25437ed3842bf60c4b9866ea1b0e53
Esther Ndosi <estherndosi24@gmail.com>
2024-02-15 00:19:08 -0800

    Print diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints line diagonally according to a given value
+ * @n: times to print
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+
+	}
+}
