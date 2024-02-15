0de34c1d3c80db015004d2c181a10c63e02efc5b
Esther Ndosi <estherndosi24@gmail.com>
2024-02-15 00:50:21 -0800

    FizzBuzz Program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz program
+ */
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("%s ", "FizzBuzz");
+		else if (i % 3 == 0)
+			printf("%s ", "Fizz");
+		else if(i % 5 == 0)
+			printf("%s ", "Buzz");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+}
