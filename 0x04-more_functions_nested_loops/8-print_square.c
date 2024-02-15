5dfb728d640c070ab4771c0803e24d5161ddb2b7
Esther Ndosi <estherndosi24@gmail.com>
2024-02-15 00:25:07 -0800

    Print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - prints square of character # for give size
+ * @size: size to print
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1, j <= size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
