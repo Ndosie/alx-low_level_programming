af04e457aa5f503457bd10b1b2b29a8e350a2268
Esther Ndosi <estherndosi24@gmail.com>
2024-02-15 01:24:01 -0800

    Print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,12 +10,15 @@ void print_triangle(int size)
 	int j;
 	int k;
 
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 1; j <= size - i; j++)
-			_putchar(' ');
-		for (k = 1; k <= i; k++)
-			_putchar('*');
+	if (size <= 0)
 		_putchar('\n');
-	}
+	else
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++)
+				_putchar(' ');
+			for (k = 1; k <= i; k++)
+				_putchar('*');
+			_putchar('\n');
+		}
 }
