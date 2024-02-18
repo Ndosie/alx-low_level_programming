6c3a17a090aa7df65940b8465043624ddf5cf29a
Esther Ndosi <estherndosi24@gmail.com>
2024-02-18 05:13:14 -0800

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "main.h"
 
 /**
@@ -16,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	o = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	w = write(o, text_content);
+	w = write(o, text_content, strlen(text_content));
 
 	if (o == -1 || w == -1)
 		return (-1);
