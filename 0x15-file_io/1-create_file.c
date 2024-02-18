768b3b1f7b35a9e101759cf15fab37cce4a0527b
Esther Ndosi <estherndosi24@gmail.com>
2024-02-18 05:20:23 -0800

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	o = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	w = write(o, text_content, strlen(text_content));
+
+	if (text_content != NULL)
+		w = write(o, text_content, strlen(text_content));
 
 	if (o == -1 || w == -1)
 		return (-1);
