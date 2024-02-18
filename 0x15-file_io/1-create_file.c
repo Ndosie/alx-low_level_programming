b9bd0c1bfa2306c1ea188b257fd34e3a68439de3
Esther Ndosi <estherndosi24@gmail.com>
2024-02-18 04:45:56 -0800

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,26 @@
 #include "main.h"
 
 /**
+ * create_file - creates file with content
+ * @filename: name of the file
+ * @text_content: content to add to a file
+ *
+ * Return: 1 success, -1 failure
  */
+int create_file(const char *filename, char *text_content)
+{
+	int o;
+	ssize_t w;
+
+	if (filename == NULL)
+		return (-1);
+
+	o = open(filename, O_CREAT, O_WRONLY);
+	w = write(o, text_content);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+	return (1);
+}
