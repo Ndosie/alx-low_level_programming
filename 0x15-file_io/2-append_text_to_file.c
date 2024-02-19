9bfd9681605e82b91993c3d31770a3b5ca57a21f
Esther Ndosi <estherndosi24@gmail.com>
2024-02-18 23:13:37 -0800

    Append to a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	o = open(filename, O_APPEND | O_WRONLY);
 
-	if (text_contents != NULL)
+	if (text_content != NULL)
 		w = write(o, text_content, strlen(text_content));
 
 	if (o == -1 || w == -1)
