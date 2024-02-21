bfd9be19fdef15926d49f476a9f6baefbaef606e
Esther Ndosi <estherndosi24@gmail.com>
2024-02-21 02:41:04 -0800

    Print array element

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - prints elements according to a given number
+ * @a: array
+ * @n: number of elements
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (i < n)
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+		i++;
+	}
+}
