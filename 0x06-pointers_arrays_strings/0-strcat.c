06972063b3d1830f6c4481e63fa0b6c8160d4a1a
Esther Ndosi <estherndosi24@gmail.com>
2024-03-04 00:20:00 -0800

    Appending string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcat - appends string to another string
+ * @dest: string to append to
+ * @src: string to append
+ *
+ * Return: final string
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return (dest);
+}
