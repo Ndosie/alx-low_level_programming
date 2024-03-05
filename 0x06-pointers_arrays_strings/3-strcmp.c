574bd1b44b2b7660fb2962f2595c1d2162e9fb82
Esther Ndosi <estherndosi24@gmail.com>
2024-03-04 23:43:17 -0800

    Comparing strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares the two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0, negative number and positive number
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int r;
+
+	i = 0;
+	r = 0;
+	while (s1[i])
+	{
+		if (s2[i] == '\0')
+		{
+			i++;
+			r = s1[i];
+			break;
+		}
+		if (s1[i] != s2[i])
+		{
+
+			r = s1[i] - s2[i];
+			break;
+		}
+		i++;
+	}
+
+	if (s2[i])
+		r = s2[i];
+	return (r);
+}
