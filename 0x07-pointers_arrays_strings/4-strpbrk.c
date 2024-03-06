a89cb7f270288bbb4c39552f3f9d5f674b8c6dce
Esther Ndosi <estherndosi24@gmail.com>
2024-03-06 01:49:24 -0800

    Search a string and return pointer

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk  - searches the string and return a pointer to the 1st byte
+ * @s: string
+ * @accept: bytes to search
+ *
+ * Return: pointer to the first occurance or null
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		i = 0;
+		while (accept[i])
+		{
+			if (s == accept[i])
+				return (s);
+			i++;
+		}
+		s++;
+	}
+
+	if (*s == '\0')
+		return ('\0');
+}
