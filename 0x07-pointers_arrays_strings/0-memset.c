ef0e86ee1cb6e489dbd0cc79636dcb15037609ae
Esther Ndosi <estherndosi24@gmail.com>
2024-03-05 22:17:12 -0800

    Filling pointed memory with byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills the pointer s with the first n bytes with a constant byte
+ * @s: pointer to fill
+ * @b: constant byte
+ * @n: number of bytes
+ *
+ * Return: the filled pointer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+		*(s + i) = b;
+
+	return (s);
+}
