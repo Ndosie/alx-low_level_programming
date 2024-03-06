d5e4efb592fe976a19aab257aac30b5e1b2541c7
Esther Ndosi <estherndosi24@gmail.com>
2024-03-05 22:57:09 -0800

    Returns pointer to the 1st occurance of a char in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strchr - returns the pointer to 1st occurance of a character in a string
+ * @s: string
+ * @c: character
+ *
+ * Return: pointer the first occurance of a character
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *p = '\0';
+
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			p = *(s + i);
+			break;
+		}
+	}
+
+	if (p == '\0')
+		p = *(s + i);
+
+	return (p);
+}
