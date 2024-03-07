6b490b9f64f0d1dcc6fadcd0b8b313a789299bed
Esther Ndosi <estherndosi24@gmail.com>
2024-03-07 00:18:03 -0800

    Searching substring in a string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strstr - searches a substring in a string
+ * @haystack: string
+ * @needle: substring
+ *
+ * Return: the pointer thr 1st letter of substring or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int l;
+	int i;
+	int j = 0;
+
+	while (*needle)
+	{
+		l++;
+		needle++;
+	}
+
+	while (1)
+	{
+		if (*haystack == needle[0])
+		{
+			for (i = 0; needle[i] != '\0'; i++)
+			{
+				if (*haystack == *needle[i])
+				{
+					j++;
+					haystack++;
+				}
+			}
+			if (l == j)
+				return (haystack - j);
+		}
+
+		if (*haystack == '\0')
+			return ('\0');
+		haystack++;
+	}
+
+}
