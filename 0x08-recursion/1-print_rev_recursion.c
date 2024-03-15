327f56388a98c653632e2cc0deaae7c0b30d32c4
Esther Ndosi <estherndosi24@gmail.com>
2024-03-15 01:09:33 -0700

    Print string in reverse recursively

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,7 +8,11 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_print_rev_recursion(s++);
+		s++;
+		_print_rev_recursion(s);
 	}
-	_putchar(*s);
+	if (*s == '\0')
+		return;
+	else
+		_putchar(*s);
 }
