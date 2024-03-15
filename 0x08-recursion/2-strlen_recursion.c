48584967947047140983b9026636d76beb69c435
Esther Ndosi <estherndosi24@gmail.com>
2024-03-15 01:55:56 -0700

    Get string length recursively

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,11 +9,10 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (*s != '\0')
-	{
-		i++;
-		_strlen_recursion(s + 1);
-	}
+	if (*s == '\0')
+		return (0);
+	else
+		return (1);
 
-	return (i);
+	return (i + _strlen_recursion(s + 1)) ;
 }
