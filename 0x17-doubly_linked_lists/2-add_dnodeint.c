6b583b5622c217c54f146cd9751fca7f9602060d
Esther Ndosi <estherndosi24@gmail.com>
2024-03-21 09:50:03 -0700

    Adding element at the head of doubly linked list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds double node at the head of a doubly linked lists
+ * @head: pointer to pointer of the head of linked list
+ * @n: number of a node
+ *
+ * Return: address of a new node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return;
+
+	new->n = n;
+	*head->prev = new;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
