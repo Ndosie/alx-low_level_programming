765c0fa8c00a31893816a7e899e548964a1b5586
Esther Ndosi <estherndosi24@gmail.com>
2024-04-17 02:31:34 -0700

    Creating hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,11 +9,12 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
+
 	if (!table)
-		return NULL;
+		return (NULL);
 
 	table->size = size;
 	table->array = calloc(size, sizeof(hash_node_t));
 
-	return table;	
+	return (table);
 }
