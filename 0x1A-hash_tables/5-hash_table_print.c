4953b6a649cd812811dc7d251cf9523e40495a01
Esther Ndosi <estherndosi24@gmail.com>
2024-04-17 06:31:29 -0700

    Print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints hash table
+ * @ht: hash table to print
+ *
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *item;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		item = ht->array[i];
+		while (item != NULL)
+		{
+			printf("%s : %s", item->key, item->value);
+			item = item->next;
+
+			if (item != NULL)
+				printf(",");
+		}
+	}
+	 printf("{\n");
+}
