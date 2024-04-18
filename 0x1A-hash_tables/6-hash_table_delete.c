412d54891b0dd0684fa8e4da8c362ac36ec040ee
Esther Ndosi <estherndosi24@gmail.com>
2024-04-18 02:13:57 -0700

    Delete a hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,46 @@
+#include "hash_tables.h"
+
+/**
+ * free_recursively - frees linked node from a hash table
+ * @item: linked node
+ *
+ */
+void free_recursively(hash_node_t *item)
+{
+	if (item->next != NULL)
+		free_recursively(item->next);
+
+	free(item->key);
+	free(item->value);
+	free(item);
+}
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to delete
+ *
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *item;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			item = ht->array[i];
+			if (item->next != NULL)
+			{
+				free_recursively(item->next);
+			}
+			free(item->key);
+			free(item->value);
+			free(item);
+		}
+	}
+
+}
