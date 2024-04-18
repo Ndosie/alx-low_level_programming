12e01e5803db448dc8a5c80f1e46afdd9b910764
Esther Ndosi <estherndosi24@gmail.com>
2024-04-18 00:10:14 -0700

    Printing hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -19,12 +19,9 @@ void hash_table_print(const hash_table_t *ht)
 		item = ht->array[i];
 		while (item != NULL)
 		{
-			printf("%s : %s", item->key, item->value);
+			printf("'%s' : '%s',", item->key, item->value);
 			item = item->next;
-
-			if (item != NULL)
-				printf(",");
 		}
 	}
-	 printf("{\n");
+	printf("}\n");
 }
